b013b93b69fdfe9b136c6ba50a71a3921b1a6f36
Simon Bartlett <simon@securitycompass.com>
2016-01-18 16:33:51 -0500

    Bumped version to 0.1.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = [
 
 setup(
     name='django-multi_import',
-    version='0.1.3',
+    version='0.1.4',
     author='Simon Bartlett',
     author_email='simon@securitycompass.com',
     packages=find_packages(),
