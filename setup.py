c38001228571bb993c772b5207e026422f7c3131
Simon Bartlett <simon@securitycompass.com>
2020-11-05 12:37:46 -0500

    Bumped verion to 1.2.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = ["chardet", "tablib", "six"]
 
 setup(
     name="django-multi_import",
-    version="1.2.3",
+    version="1.2.4",
     author="Simon Bartlett",
     author_email="simon@securitycompass.com",
     packages=find_packages(),
