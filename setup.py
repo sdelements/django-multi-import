5cdeb2e8868d5160144c9672e8a0166344f75788
Kelly Bell <kbell@securitycompass.com>
2021-02-04 10:44:59 -0500

    Bumped version to 1.2.7

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = ["chardet", "tablib", "six"]
 
 setup(
     name="django-multi_import",
-    version="1.2.6",
+    version="1.2.7",
     author="Simon Bartlett",
     author_email="simon@securitycompass.com",
     packages=find_packages(),
