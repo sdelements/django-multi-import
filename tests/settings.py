87f896a3ba7f0003d5b4328561b7903fd5dde45f
Sunny Rangnani <srangnani@securitycompass.com>
2024-06-04 21:19:37 -0400

    tests structure

--- tests/settings.py
+++ tests/settings.py
@@ -1,12 +1,46 @@
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
+DEBUG = True
+ADMINS = ()
+MANAGERS = ADMINS
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "tests_db",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    }
+}
+TIME_ZONE = "America/Chicago"
+USE_TZ = True
+LANGUAGE_CODE = "en-us"
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+
+SECRET_KEY = "foobar"  # noqa: S105
 
 MIDDLEWARE_CLASSES = (
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
 )
 
-INSTALLED_APPS = ("tests",)
+INSTALLED_APPS = ("multi_import", "tests")
 
-SITE_ID = 1
-
-SECRET_KEY = "this-is-just-for-tests-so-not-that-secret"  # noqa
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console"],
+            "level": "WARNING",
+            "propagate": True,
+        },
+    },
+}
